b53ac9e629f38c2f3baa126bba16806521b86ffa
Kanguk Lee <p51lee@kaist.ac.kr>
2025-07-15 17:46:45 +0900

    Edit unit test

--- src/astutil/tests.rs
+++ src/astutil/tests.rs
@@ -40,16 +40,11 @@ fn test_append_after_item() {
     let item_code = "fn new_item() {}";
     let expected_code = format!("{}\n{}", orig_code, item_code);
 
-    let target_span = Span::new(BytePos(0), BytePos(12), SyntaxContext::root(), None);
-
     run_compiler(orig_code, |tcx| {
         let mut krate = parse_crate(orig_code.to_string());
-        println!("Original crate: {}", crate_to_string_for_macros(&krate));
-        // let first_item = krate.items.first().unwrap();
-        // println!("Original crate: {:#?}", krate);
-        // print_span_info(&first_item.span);
+        let item_span = krate.items.first().unwrap().span;
         let suggestion = AstSuggestion {
-            span: target_span,
+            span: item_span,
             action: astutil::AstEdit::AppendAfterItem(item!("{}", item_code)),
         };
         let mut mut_visitor = TransformVisitor::new(tcx, vec![suggestion]);
@@ -64,16 +59,11 @@ fn test_replace_item() {
     let item_code = "fn new_item() {}";
     let expected_code = item_code;
 
-    let target_span = Span::new(BytePos(0), BytePos(12), SyntaxContext::root(), None);
-
     run_compiler(orig_code, |tcx| {
         let mut krate = parse_crate(orig_code.to_string());
-        println!("Original crate: {}", crate_to_string_for_macros(&krate));
-        // let first_item = krate.items.first().unwrap();
-        // println!("Original crate: {:#?}", krate);
-        // print_span_info(&first_item.span);
+        let item_span = krate.items.first().unwrap().span;
         let suggestion = AstSuggestion {
-            span: target_span,
+            span: item_span,
             action: astutil::AstEdit::ReplaceItem(item!("{}", item_code)),
         };
         let mut mut_visitor = TransformVisitor::new(tcx, vec![suggestion]);
@@ -87,22 +77,19 @@ fn test_remove_field_def() {
     let orig_code = "struct Human { brain: i32, heart: i32 }";
     let expected_code = "struct Human { heart: i32 }";
 
-    // Struct item's span
-    let target_span = Span::new(BytePos(0), BytePos(39), SyntaxContext::root(), None);
-    // Field definition's span
-    let field_span = Span::new(BytePos(15), BytePos(25), SyntaxContext::root(), None);
-
     run_compiler(orig_code, |tcx| {
         let mut krate = parse_crate(orig_code.to_string());
-        // let ItemKind::Struct(VariantData::Struct { fields, .. }, _) =
-        //     &krate.items.first().unwrap().kind
-        // else {
-        //     panic!("Expected a struct item");
-        // };
+        let ItemKind::Struct(VariantData::Struct { fields, .. }, _) =
+            &krate.items.first().unwrap().kind
+        else {
+            panic!("Expected a struct item");
+        };
+        let item_span = krate.items.first().unwrap().span;
+        let field_span = fields.first().unwrap().span;
         // print_span_info(&krate.items.first().unwrap().span);
         // print_span_info(&fields.first().unwrap().span);
         let suggestion = AstSuggestion {
-            span: target_span,
+            span: item_span,
             action: astutil::AstEdit::RemoveFieldDef(field_span),
         };
         let mut mut_visitor = TransformVisitor::new(tcx, vec![suggestion]);
@@ -118,16 +105,14 @@ fn test_prepend_to_block() {
     let orig_code = format!("fn main() {{ {} }}", orig_stmt);
     let expected_code = format!("fn main() {{ {} {} }}", prep_stmt, orig_stmt);
 
-    let target_span = Span::new(BytePos(10), BytePos(24), SyntaxContext::root(), None);
-
     run_compiler(&orig_code, |tcx| {
         let mut krate = parse_crate(orig_code.to_string());
-        // let ItemKind::Fn(box Fn { body: Some(b), .. }) = &krate.items.first().unwrap().kind else {
-        //     panic!("Expected a function item");
-        // };
-        // print_span_info(&b.span);
+        let ItemKind::Fn(box Fn { body: Some(b), .. }) = &krate.items.first().unwrap().kind else {
+            panic!("Expected a function item");
+        };
+        let block_span = b.span;
         let suggestion = AstSuggestion {
-            span: target_span,
+            span: block_span,
             action: astutil::AstEdit::PrependToBlock(stmt!("{}", prep_stmt)),
         };
         let mut mut_visitor = TransformVisitor::new(tcx, vec![suggestion]);
@@ -143,22 +128,16 @@ fn test_replace_stmt() {
     let orig_code = format!("fn main() {{ {} }}", orig_stmt);
     let expected_code = format!("fn main() {{ {} }}", repl_stmt);
 
-    let target_span = Span::new(BytePos(12), BytePos(22), SyntaxContext::root(), None);
-
     run_compiler(&orig_code, |tcx| {
         let mut krate = parse_crate(orig_code.to_string());
-        // let ItemKind::Fn(box rustc_ast::Fn { body: Some(b), .. }) =
-        //     &krate.items.first().unwrap().kind
-        // else {
-        //     panic!("Expected a function item");
-        // };
-        // println!(
-        //     "Span of the first statement: {:?}",
-        //     b.stmts.first().unwrap().span.data()
-        // );
-        // print_span_info(&b.stmts.first().unwrap().span);
+        let ItemKind::Fn(box rustc_ast::Fn { body: Some(b), .. }) =
+            &krate.items.first().unwrap().kind
+        else {
+            panic!("Expected a function item");
+        };
+        let stmt_span = b.stmts.first().unwrap().span;
         let suggestion = AstSuggestion {
-            span: target_span,
+            span: stmt_span,
             action: astutil::AstEdit::ReplaceStmt(stmt!("{}", repl_stmt)),
         };
         let mut mut_visitor = TransformVisitor::new(tcx, vec![suggestion]);
@@ -172,12 +151,16 @@ fn test_remove_stmt() {
     let orig_code = format!("fn main() {{ let x = 5; }}");
     let expected_code = format!("fn main() {{ }}");
 
-    let target_span = Span::new(BytePos(12), BytePos(22), SyntaxContext::root(), None);
-
     run_compiler(&orig_code, |tcx| {
         let mut krate = parse_crate(orig_code.to_string());
+        let ItemKind::Fn(box rustc_ast::Fn { body: Some(b), .. }) =
+            &krate.items.first().unwrap().kind
+        else {
+            panic!("Expected a function item");
+        };
+        let stmt_span = b.stmts.first().unwrap().span;
         let suggestion = AstSuggestion {
-            span: target_span,
+            span: stmt_span,
             action: astutil::AstEdit::RemoveStmt,
         };
         let mut mut_visitor = TransformVisitor::new(tcx, vec![suggestion]);
@@ -192,19 +175,17 @@ fn test_replace_pat() {
     let orig_code = format!("fn main() {{ let x = 5; }}");
     let expected_code = format!("fn main() {{ let Chim(chak) = 5; }}");
 
-    let target_span = Span::new(BytePos(16), BytePos(17), SyntaxContext::root(), None);
-
     run_compiler(&orig_code, |tcx| {
         let mut krate = parse_crate(orig_code.to_string());
-        // let ItemKind::Fn(box Fn { body: Some(b), .. }) = &krate.items.first().unwrap().kind else {
-        //     panic!("Expected a function item");
-        // };
-        // let StmtKind::Let(local) = &b.stmts.first().unwrap().kind else {
-        //     panic!("Expected a let statement");
-        // };
-        // println!("Span of the pat: {:?}", print_span_info(&local.pat.span));
+        let ItemKind::Fn(box Fn { body: Some(b), .. }) = &krate.items.first().unwrap().kind else {
+            panic!("Expected a function item");
+        };
+        let StmtKind::Let(local) = &b.stmts.first().unwrap().kind else {
+            panic!("Expected a let statement");
+        };
+        let pat_span = local.pat.span;
         let suggestion = AstSuggestion {
-            span: target_span,
+            span: pat_span,
             action: astutil::AstEdit::ReplacePat(pat!("{}", repl_pat)),
         };
         let mut mut_visitor = TransformVisitor::new(tcx, vec![suggestion]);
@@ -219,22 +200,17 @@ fn test_replace_expr() {
     let orig_code = format!("fn main() {{ let x = 5; }}");
     let expected_code = format!("fn main() {{ let x = 10; }}");
 
-    let target_span = Span::new(BytePos(20), BytePos(21), SyntaxContext::root(), None);
-
     run_compiler(&orig_code, |tcx| {
         let mut krate = parse_crate(orig_code.to_string());
-        // let ItemKind::Fn(box Fn { body: Some(b), .. }) = &krate.items.first().unwrap().kind else {
-        //     panic!("Expected a function item");
-        // };
-        // let StmtKind::Let(local) = &b.stmts.first().unwrap().kind else {
-        //     panic!("Expected a let statement");
-        // };
-        // println!(
-        //     "Span of the expression: {:?}",
-        //     print_span_info(&local.kind.init().unwrap().span)
-        // );
+        let ItemKind::Fn(box Fn { body: Some(b), .. }) = &krate.items.first().unwrap().kind else {
+            panic!("Expected a function item");
+        };
+        let StmtKind::Let(local) = &b.stmts.first().unwrap().kind else {
+            panic!("Expected a let statement");
+        };
+        let expr_span = local.kind.init().unwrap().span;
         let suggestion = AstSuggestion {
-            span: target_span,
+            span: expr_span,
             action: astutil::AstEdit::ReplaceExpr(expr!("{}", repl_expr)),
         };
         let mut mut_visitor = TransformVisitor::new(tcx, vec![suggestion]);
@@ -248,24 +224,21 @@ fn test_remove_expr_field() {
     let orig_code = format!("fn main() {{ let x = Human {{ brain: 1, heart: 1 }}; }}");
     let expected_code = format!("fn main() {{ let x = Human {{ heart: 1 }}; }}");
 
-    let target_span = Span::new(BytePos(20), BytePos(48), SyntaxContext::root(), None);
-    let field_span = Span::new(BytePos(28), BytePos(36), SyntaxContext::root(), None);
-
     run_compiler(&orig_code, |tcx| {
         let mut krate = parse_crate(orig_code.to_string());
-        // let ItemKind::Fn(box Fn { body: Some(b), .. }) = &krate.items.first().unwrap().kind else {
-        //     panic!("Expected a function item");
-        // };
-        // let StmtKind::Let(local) = &b.stmts.first().unwrap().kind else {
-        //     panic!("Expected a let statement");
-        // };
-        // let ExprKind::Struct(struct_expr) = &local.kind.init().unwrap().kind else {
-        //     panic!("Expected a struct expression");
-        // };
-        // print_span_info(&local.kind.init().unwrap().span); // Expression span
-        // print_span_info(&struct_expr.fields.first().unwrap().span); // Field span
+        let ItemKind::Fn(box Fn { body: Some(b), .. }) = &krate.items.first().unwrap().kind else {
+            panic!("Expected a function item");
+        };
+        let StmtKind::Let(local) = &b.stmts.first().unwrap().kind else {
+            panic!("Expected a let statement");
+        };
+        let ExprKind::Struct(struct_expr) = &local.kind.init().unwrap().kind else {
+            panic!("Expected a struct expression");
+        };
+        let expr_span = local.kind.init().unwrap().span;
+        let field_span = struct_expr.fields.first().unwrap().span;
         let suggestion = AstSuggestion {
-            span: target_span,
+            span: expr_span,
             action: astutil::AstEdit::RemoveExprField(field_span),
         };
         let mut mut_visitor = TransformVisitor::new(tcx, vec![suggestion]);
@@ -279,22 +252,17 @@ fn test_remove_field_attr() {
     let orig_code = "struct Human {\n#[rotten]\nbrain: i32,\nheart: i32 }";
     let expected_code = "struct Human {\nbrain: i32,\nheart: i32 }";
 
-    // Struct item's span
-    let target_span = Span::new(BytePos(25), BytePos(35), SyntaxContext::root(), None);
-    // Field definition's span
-    let attr_span = Span::new(BytePos(15), BytePos(24), SyntaxContext::root(), None);
-
     run_compiler(orig_code, |tcx| {
         let mut krate = parse_crate(orig_code.to_string());
-        // let ItemKind::Struct(VariantData::Struct { fields, .. }, _) =
-        //     &krate.items.first().unwrap().kind
-        // else {
-        //     panic!("Expected a struct item");
-        // };
-        // print_span_info(&fields.first().unwrap().span);
-        // print_span_info(&fields.first().unwrap().attrs.first().unwrap().span);
+        let ItemKind::Struct(VariantData::Struct { fields, .. }, _) =
+            &krate.items.first().unwrap().kind
+        else {
+            panic!("Expected a struct item");
+        };
+        let field_def_span = fields.first().unwrap().span;
+        let attr_span = fields.first().unwrap().attrs.first().unwrap().span;
         let suggestion = AstSuggestion {
-            span: target_span,
+            span: field_def_span,
             action: astutil::AstEdit::RemoveFieldAttr(attr_span),
         };
         let mut mut_visitor = TransformVisitor::new(tcx, vec![suggestion]);
